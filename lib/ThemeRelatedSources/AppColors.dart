0398beecb0d2e9301dd3a19c6d34e39f04c9256f
Cem Bozkurt <cem.bozkurt.3493@gmail.com>
2022-12-08 23:44:17 +0300

    Diyetisyen danışan bilgisi sayfası

--- lib/ThemeRelatedSources/AppColors.dart
+++ lib/ThemeRelatedSources/AppColors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/animation.dart';
 
 class AppColors {
   static Color mainBackgrounColor = Color.fromRGBO(255, 139, 113, 1);
+  static Color danisanCardBackgroundColor = Color.fromARGB(192, 255, 172, 151);
 }
