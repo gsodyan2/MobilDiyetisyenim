a3d7f5e5b5baf4a0dc4268d9288b897a64fe3a3f
Cem Bozkurt <cem.bozkurt.3493@gmail.com>
2022-12-24 16:45:38 +0300

    visual fix

--- lib/DiyetisyenPages/DiyetisyenReviewMealPage.dart
+++ lib/DiyetisyenPages/DiyetisyenReviewMealPage.dart
@@ -32,85 +32,86 @@ class _DiyetisyenReviewMealPage extends State<DiyetisyenReviewMealPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text("Mobil Diyetisyenim"),
-          backgroundColor: AppColors.mainBackgrounColor),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            // Yemeğin Resmi
-            Image.network(
-              widget.mealPhotoUrl,
-              height: 100,
-            ),
-            EmptyBox(width: 1, height: 20),
-            // Danışanın adı
-            Text(widget.patientName,
-                style:
-                    const TextStyle(fontSize: 13, fontWeight: FontWeight.bold)),
-            // Tarih
-            Text(widget.date,
-                style:
-                    const TextStyle(fontSize: 13, fontWeight: FontWeight.bold)),
-            EmptyBox(width: 1, height: 20),
-            // Not girme kısmı
-            TextField(
-              maxLines: 5,
-              onChanged: (value) {
-                textNote = value;
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Yorumunuzu Giriniz',
+        appBar: AppBar(
+            title: Text("Mobil Diyetisyenim"),
+            backgroundColor: AppColors.mainBackgrounColor),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              // Yemeğin Resmi
+              Image.network(
+                widget.mealPhotoUrl,
+                height: 100,
               ),
-            ),
-            EmptyBox(width: 1, height: 10),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      // Nutri approving meal.
-                      widget.userController
-                          .UpdateMealInfo(widget.mealId, true, textNote);
-                      print("meal approved  mealID = " + widget.mealId);
-                      widget.mainPageWidget.setState(() {
-                        widget.mainPageWidget.isPageSet = false;
-                      });
-                      Navigator.pop(context);
-                    },
-                    child: Image.asset(
-                      "assets/approve_sign.png",
-                      height: 50,
-                      width: 50,
+              EmptyBox(width: 1, height: 20),
+              // Danışanın adı
+              Text(widget.patientName,
+                  style: const TextStyle(
+                      fontSize: 13, fontWeight: FontWeight.bold)),
+              // Tarih
+              Text(widget.date,
+                  style: const TextStyle(
+                      fontSize: 13, fontWeight: FontWeight.bold)),
+              EmptyBox(width: 1, height: 20),
+              // Not girme kısmı
+              TextField(
+                maxLines: 5,
+                onChanged: (value) {
+                  textNote = value;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Yorumunuzu Giriniz',
+                ),
+              ),
+              EmptyBox(width: 1, height: 10),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        // Nutri approving meal.
+                        widget.userController
+                            .UpdateMealInfo(widget.mealId, true, textNote);
+                        print("meal approved  mealID = " + widget.mealId);
+                        widget.mainPageWidget.setState(() {
+                          widget.mainPageWidget.isPageSet = false;
+                        });
+                        Navigator.pop(context);
+                      },
+                      child: Image.asset(
+                        "assets/approve_sign.png",
+                        height: 50,
+                        width: 50,
+                      ),
                     ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      // Nutri not approving meal.
-                      widget.userController
-                          .UpdateMealInfo(widget.mealId, false, textNote);
-                      print("meal not approved  mealID = " + widget.mealId);
-                      widget.mainPageWidget.setState(() {
-                        widget.mainPageWidget.isPageSet = false;
-                      });
+                    TextButton(
+                      onPressed: () {
+                        // Nutri not approving meal.
+                        widget.userController
+                            .UpdateMealInfo(widget.mealId, false, textNote);
+                        print("meal not approved  mealID = " + widget.mealId);
+                        widget.mainPageWidget.setState(() {
+                          widget.mainPageWidget.isPageSet = false;
+                        });
 
-                      Navigator.pop(context);
-                    },
-                    child: Image.asset(
-                      "assets/decline_sign.png",
-                      height: 65,
-                    ),
-                  )
-                ],
-              ),
-            )
-          ]),
+                        Navigator.pop(context);
+                      },
+                      child: Image.asset(
+                        "assets/decline_sign.png",
+                        height: 65,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ]),
+          ),
         ),
-      ),
-    );
+        resizeToAvoidBottomInset: false);
   }
 }
